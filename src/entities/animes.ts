083bbfc4c1037f827889b287b942dc4515929345
José Lucas <josluc2016@gmail.com>
2023-04-24 12:38:09 -0300

    feat: started

---
+++ src/entities/animes.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from "crypto";
+
+export class Anime {
+    public readonly id!: string;
+    public name!: string;
+    public episodes!: number;
+
+    constructor(props: Omit<Anime, 'id'>, id?: string) {
+        Object.assign(this, props);
+
+        if (!id) this.id = randomUUID();
+    }
+}
